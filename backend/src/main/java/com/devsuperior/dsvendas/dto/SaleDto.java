47865c534ce5af638ebe8732e688992b7a7c4de6
Baldez Jr <abaldezjr@gmail.com>
2021-11-05 20:41:42 -0300

    pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDto.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDto.java
@@ -1,44 +1,39 @@
-package com.devsuperior.dsvendas.entities;
+package com.devsuperior.dsvendas.dto;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import com.devsuperior.dsvendas.entities.Sale;
 
-@Entity
-@Table(name = "tb_sales")
-public class Sale {
+public class SaleDto {
 	
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 	
-	@ManyToOne
-	@JoinColumn(name = "seller_id")
-	private Seller seller;
+	private SellerDto seller;
 	
-	public Sale() {
+	public SaleDto() {
 		
 	}
 
-	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
-		super();
+	public SaleDto(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDto seller) {
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
 		this.amount = amount;
 		this.date = date;
 		this.seller = seller;
+	}
+	
+	public SaleDto(Sale sale) {
+		this.id = sale.getId();
+		this.visited = sale.getVisited();
+		this.deals = sale.getDeals();
+		this.amount = sale.getAmount();
+		this.date = sale.getDate();
+		this.seller = new SellerDto(sale.getSeller());
 	}
 
 	public Long getId() {
@@ -81,11 +76,11 @@ public class Sale {
 		this.date = date;
 	}
 
-	public Seller getSeller() {
+	public SellerDto getSeller() {
 		return seller;
 	}
 
-	public void setSeller(Seller seller) {
+	public void setSeller(SellerDto seller) {
 		this.seller = seller;
 	}
 	
